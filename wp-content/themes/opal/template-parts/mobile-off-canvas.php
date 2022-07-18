e88a87947e3eabaa7098e69584d38d75938c83cf
StYankov <st.yankov00@gmail.com>
2022-07-18 19:08:38 +0300

    Schedule a visit button in mobile nav menu

--- wp-content/themes/opal/template-parts/mobile-off-canvas.php
+++ wp-content/themes/opal/template-parts/mobile-off-canvas.php
@@ -10,6 +10,21 @@
 
 <nav class="mobile-off-canvas-menu off-canvas position-left" id="<?php foundationpress_mobile_menu_id(); ?>" data-off-canvas data-auto-focus="false" role="navigation">
 	<?php foundationpress_mobile_nav(); ?>
+
+	<div class="quick-nav">
+		<?php if( get_theme_mod( 'opal_header_phone' ) ) : ?>
+			<a class="top-bar-phone" href="tel:<?= get_theme_mod( 'opal_header_phone' ) ?>">
+				<i class="fa-solid fa-phone"></i>
+			</a>
+		<?php endif; ?>
+
+		<?php if( get_theme_mod( 'opal_header_contact_form' ) ) : ?>
+			<button type="button" class="button schedule-visit" data-open="schedule-call">
+				<i class="fa-solid fa-calendar-check"></i>
+				<?php _e( 'schedule a visit', 'golfvilla' ); ?>
+			</button>
+		<?php endif; ?>
+	</div>
 </nav>
 
 <div class="off-canvas-content" data-off-canvas-content>
